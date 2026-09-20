d781359132a4ee02b3875205b92f52caab81b7ae
kwkwiwinbhaj-del <kwkwiwinbhaj@gmail.com>
2026-09-20 11:38:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
  
-bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None, debug_guilds=[GUILD_ID] if GUILD_ID else None)
  
  
 class Database:
